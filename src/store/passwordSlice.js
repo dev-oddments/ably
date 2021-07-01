f6d101bb7a5ae43d0126444f1bfc0c256b17ad57
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-01 23:49:25 +0900

    feat: passwordSlice 전역 상태와 action을 정의하고 테스트를 작성한다
    * 비밀번호 찾기 로직에 필요한 부분 모아둠

---
+++ src/store/passwordSlice.js
@@ -0,0 +1,207 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { displayModal } from '@store';
+import { MODALS } from '@utils/constants';
+import {
+  getAuthCodeApi,
+  postAuthCodeCompareApi,
+  patchAuthCodeChangeApi,
+} from '@repository/baseRepository';
+
+import {
+  EMAIL_EXIST,
+  AUTH_CODE_CHECK,
+  PASSWORD_CHANGE,
+} from '@utils/constants';
+
+const passwordReducer = createSlice({
+  name: 'password',
+  initialState: {
+    route: EMAIL_EXIST,
+    issueToken: '',
+    confirmToken: '',
+    remainTime: 0,
+    timerId: '',
+  },
+  reducers: {
+    setRoute(state, { payload: { route } }) {
+      return {
+        ...state,
+        route,
+      };
+    },
+    setIssueToken(state, { payload: { issueToken } }) {
+      return {
+        ...state,
+        issueToken,
+      };
+    },
+    setConfirmToken(state, { payload: { confirmToken } }) {
+      return {
+        ...state,
+        confirmToken,
+      };
+    },
+    setRemainTime(state, { payload: { remainTime } }) {
+      return {
+        ...state,
+        remainTime,
+      };
+    },
+    setTimerId(state, { payload: { timerId } }) {
+      return {
+        ...state,
+        timerId,
+      };
+    },
+  },
+});
+
+export const {
+  setRoute,
+  setIssueToken,
+  setConfirmToken,
+  setRemainTime,
+  setTimerId,
+} = passwordReducer.actions;
+export default passwordReducer.reducer;
+
+export const resetTime = () => (dispatch, getState) => {
+  const {
+    password: { timerId },
+  } = getState();
+
+  clearInterval(timerId);
+  dispatch(setTimerId({ timerId: '' }));
+  dispatch(setRemainTime({ remainTime: 0 }));
+};
+
+export const startTime =
+  ({ remainMillisecond }) =>
+  (dispatch, getState) => {
+    dispatch(resetTime());
+    dispatch(setRemainTime({ remainTime: Number(remainMillisecond / 1000) }));
+
+    const timerId = setInterval(() => {
+      const {
+        password: { remainTime },
+      } = getState();
+
+      if (remainTime === 0) {
+        dispatch(
+          displayModal({
+            modalName: MODALS.ALERT_MODAL,
+            content: `시간이 만료되어 이메일을 다시 입력해주셔야 합니다!`,
+          })
+        );
+        dispatch(setRoute({ route: EMAIL_EXIST }));
+        return dispatch(resetTime());
+      }
+      return dispatch(setRemainTime({ remainTime: remainTime - 1 }));
+    }, 1000);
+
+    dispatch(setTimerId({ timerId }));
+  };
+
+export const checkEmailExist =
+  ({ email }) =>
+  async (dispatch) => {
+    try {
+      const {
+        data: { issueToken, remainMillisecond },
+      } = await getAuthCodeApi({ email });
+
+      dispatch(setRoute({ route: AUTH_CODE_CHECK }));
+      dispatch(setIssueToken({ issueToken }));
+      dispatch(startTime({ remainMillisecond }));
+    } catch ({
+      response: {
+        data: {
+          error: { message },
+        },
+      },
+    }) {
+      dispatch(
+        displayModal({
+          modalName: MODALS.ALERT_MODAL,
+          content: message,
+        })
+      );
+    }
+  };
+
+export const checkAuthCodeRight =
+  ({ email, authCode }) =>
+  async (dispatch, getState) => {
+    const {
+      password: { issueToken },
+    } = getState();
+
+    try {
+      const {
+        data: { confirmToken },
+      } = await postAuthCodeCompareApi({
+        param: { email, authCode, issueToken },
+      });
+
+      dispatch(setRoute({ route: PASSWORD_CHANGE }));
+      dispatch(setConfirmToken({ confirmToken }));
+      dispatch(resetTime());
+    } catch ({
+      response: {
+        data: {
+          error: { message },
+        },
+      },
+    }) {
+      dispatch(
+        displayModal({
+          modalName: MODALS.ALERT_MODAL,
+          content: message,
+        })
+      );
+    }
+  };
+
+// eslint-disable-next-line max-len
+export const changePassword =
+  ({ email, newPassword, newPasswordConfirm, history }) =>
+  async (dispatch, getState) => {
+    const {
+      password: { confirmToken },
+    } = getState();
+
+    try {
+      await patchAuthCodeChangeApi({
+        param: {
+          email,
+          confirmToken,
+          newPassword,
+          newPasswordConfirm,
+        },
+      });
+
+      history.push('/');
+      dispatch(setRoute({ route: EMAIL_EXIST }));
+      dispatch(setIssueToken({ issueToken: '' }));
+      dispatch(setConfirmToken({ confirmToken: '' }));
+      dispatch(
+        displayModal({
+          modalName: MODALS.ALERT_MODAL,
+          content: '비밀번호 변경이 완료되었습니다!',
+        })
+      );
+    } catch ({
+      response: {
+        data: {
+          error: { message },
+        },
+      },
+    }) {
+      dispatch(
+        displayModal({
+          modalName: MODALS.ALERT_MODAL,
+          content: message,
+        })
+      );
+    }
+  };
---
+++ src/store/passwordSlice.test.js
@@ -0,0 +1,141 @@
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+import configureStore from 'redux-mock-store';
+import passwordReducer, {
+  setRoute,
+  setIssueToken,
+  setConfirmToken,
+  setRemainTime,
+  setTimerId,
+  resetTime,
+  startTime,
+//   checkEmailExist,
+//   checkAuthCodeRight,
+//   changePassword,
+} from './passwordSlice';
+
+import {
+  EMAIL_EXIST,
+  AUTH_CODE_CHECK,
+} from '@utils/constants';
+
+const middleWares = [...getDefaultMiddleware()];
+const mockStore = configureStore(middleWares);
+
+describe('passwordReducer에서', () => {
+  context('각각', () => {
+    const initialState = {
+      route: EMAIL_EXIST,
+      issueToken: '',
+      confirmToken: '',
+      remainTime: 0,
+      timerId: '',
+    };
+
+    it('setRoute 이용해 route 토글할 수 있다.', () => {
+      const state = passwordReducer(
+        initialState,
+        setRoute({
+          route: AUTH_CODE_CHECK,
+        })
+      );
+
+      expect(state.route).toEqual(AUTH_CODE_CHECK);
+    });
+
+    it('setIssueToken 이용해 issueToken 값을 변경할 수 있다.', () => {
+      const state = passwordReducer(
+        initialState,
+        setIssueToken({
+          issueToken: 'iamtoken12345',
+        })
+      );
+
+      expect(state.issueToken).toEqual('iamtoken12345');
+    });
+
+    it('setConfirmToken 이용해 confirmToken 값을 변경할 수 있다.', () => {
+      const state = passwordReducer(
+        initialState,
+        setConfirmToken({
+          confirmToken: 'iamtoken12345',
+        })
+      );
+
+      expect(state.confirmToken).toEqual('iamtoken12345');
+    });
+
+    it('setRemainTime 이용해 remainTime 값을 변경할 수 있다.', () => {
+      const state = passwordReducer(
+        initialState,
+        setRemainTime({
+          remainTime: 60,
+        })
+      );
+
+      expect(state.remainTime).toEqual(60);
+    });
+
+    it('setTimerId 이용해 timerId 값을 변경할 수 있다.', () => {
+      const state = passwordReducer(
+        initialState,
+        setTimerId({
+          timerId: 'xyz',
+        })
+      );
+
+      expect(state.timerId).toEqual('xyz');
+    });
+
+    // TODO: 테스트를 제대로 하는 방법을 생각해봐야 함
+    it('resetTime 사용해 interval과 remainTime을 초기화 할 수 있다.', () => {
+      const state = passwordReducer(initialState, resetTime());
+
+      expect(state.timerId).toEqual('');
+      expect(state.remainTime).toEqual(0);
+    });
+
+    // TODO: 테스트를 제대로 하는 방법을 생각해봐야 함
+    it('startTime을 사용해 remainMillisecond를 초단위로 바꿔 remainTime이 1초씩 줄어들도록 한다.', () => {
+      const state = passwordReducer(
+        initialState,
+        startTime({ remainMillisecond: 10000 })
+      );
+
+      expect(state.timerId).toEqual('');
+      expect(state.remainTime).toEqual(0);
+    });
+
+    // TODO: 테스트 시나리오 다시 생각해봐야 함 catch 로 가는 이류를 모르겠음
+    //     it('checkEmailExist을 사용해 이메일 확인이 가능한지를 테스트한다.', async () => {
+    //       let store = mockStore({});
+
+    //       await store.dispatch(
+    //         checkEmailExist({
+    //           email: 'ably933@dummy.com',
+    //         })
+    //       );
+    //       const actionsCheckEmailExist = store.getActions();
+    //       expect(actionsCheckEmailExist[0]).toEqual();
+
+    //       await store.dispatch(
+    //         checkAuthCodeRight({
+    //           email: 'ably933@dummy.com',
+    //           authCode: actionsCheckEmailExist[0].payload.authCode,
+    //         })
+    //       );
+    //       const actionsCheckAuthCodeRight = store.getActions();
+    //       expect(actionsCheckAuthCodeRight[0]).toEqual();
+
+    //       await store.dispatch(
+    //         changePassword({
+    //           email: 'ably933@dummy.com',
+    //           confirmToken: actionsCheckEmailExist[0].payload.confirmToken,
+    //           newPassword: '!abc321#$',
+    //           newPasswordConfirm: '!abc321#$',
+    //         })
+    //       );
+    //       const actionsChangePassword = store.getActions();
+    //       expect(actionsChangePassword[0]).toEqual();
+    //     });
+  });
+});
