f899e80e03b5fa198cc050e255666a9952b38d5d
YONGJAE LEE <dev.yongjaelee@gmail.com>
2021-07-01 23:36:33 +0900

    chore: constants를 js에서 ts로 변경하고 ResetPassword위해 route 설정한다
    * resetPasswoerPage를 고려해서 constant 정의

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const MODALS: { ALERT_MODAL: string } = {
+  ALERT_MODAL: 'alertModal',
+};
+
+export const EMAIL_EXIST: number = 0;
+export const AUTH_CODE_CHECK: number = 1;
+export const PASSWORD_CHANGE: number = 2;
